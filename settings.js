e7f21e21cae6ffcbe4d46012a21e54aa34f5a624
Abhishake Pattanaik <abhisekpattanaik41@gmail.com>
2024-03-06 00:31:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '⏤➣⃟⃟🖤•∆ѵ𝒾𝒾• ⃟⃟⃟⃟😶' //ur bot name
+global.ownernumber = '919777407381' //ur owner number
+global.ownername = 'A V I I.' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "-"
+global.author = "⏤➣⃟⃟🖤•∆ѵ𝒾𝒾• ⃟⃟⃟⃟😶"
+global.creator = "919777407381@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["919777407381"] // Premium User
 global.hituet = 0
 
 //bot sett
